a6df709189f78776d81f53b5e205a2fecf9ef0c6
Naderi Farid <geekwid@gmail.com>
2022-04-05 23:45:49 +0200

    [fix] merge

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -2,20 +2,16 @@ import Component from '../Fleat/Component.js';
 import { Fleat } from '../Fleat/Fleat.js';
 import Home from '../components/Home.js';
 import Menu from '../Fleat/Menu.js';
-<<<<<<< HEAD
 import Contact from './Contact.js';
 import Players from './Players.js';
 import Merch from './Merch.js';
-=======
 import Position from '../components/Position.js';
->>>>>>> fcf9bc9f948e3dab39d40381694afdf1e34b6da7
 
 class App extends Component {
 	render() {
 		const route = window.location.pathname;
 
 		switch (route) {
-<<<<<<< HEAD
 			case '/':
 				return Fleat.createElement(this, Home, null, null);
 			case '/players':
@@ -24,10 +20,8 @@ class App extends Component {
 				return Fleat.createElement(this, Merch, null, null);			
 			case '/contact':
 				return Fleat.createElement(this, Contact, null, null);
-=======
 			case '/position':
 				return Fleat.createElement(this, Position, null, null);
->>>>>>> fcf9bc9f948e3dab39d40381694afdf1e34b6da7
 			default:
 				return Fleat.createElement(this, Home, null, null);
 		}
