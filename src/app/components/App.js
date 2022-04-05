f357334ba8c14e441185b1db38113ac9bf6865b6
nicolasperradin <nicolas.perradin.pro@gmail.com>
2022-04-05 22:14:03 +0200

    add contact, players and merch in the menu

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,6 +1,10 @@
 import Component from '../Fleat/Component.js';
 import { Fleat } from '../Fleat/Fleat.js';
+<<<<<<< Updated upstream
 import Home from '../components/Home.js';
+=======
+import Menu from '../Fleat/Menu.js';
+>>>>>>> Stashed changes
 
 class App extends Component {
 	render() {
@@ -8,7 +12,11 @@ class App extends Component {
 
 		switch (route) {
 			default:
+<<<<<<< Updated upstream
 				return Fleat.createElement(this, Home);
+=======
+				return Fleat.createElement(this, Menu, null, null);
+>>>>>>> Stashed changes
 		}
 	}
 }
